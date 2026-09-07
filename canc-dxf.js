3e3fbf4bad9b0511f14d275d405e71288a1eb269
rodoalonso-lab <rodoalonso@procepal.com>
2026-09-06 23:44:50 -0600

    Auto-upload: canc-dxf.js 2026-09-06 23:44

--- canc-dxf.js
+++ canc-dxf.js
@@ -487,6 +487,7 @@
   }
 
   var API = { leerTextos: leerTextos, parsear: parsear, resumir: resumir, alzado: alzado,
+              cotas: cotasEtiquetadas,
               empatar: empatar, indiceInsumos: indiceInsumos, limpiar: limpiar };
 
   if (typeof module !== 'undefined' && module.exports) module.exports = API;
